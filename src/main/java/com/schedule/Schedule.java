5e9b4ade75771ef03162e59dcdb8f639ad5724aa
ttytt <weijia@snut.edu.cn>
2021-08-17 09:57:08 +0800

    首次上传

--- src/main/java/com/schedule/Schedule.java
+++ src/main/java/com/schedule/Schedule.java
@@ -1,2 +1,18 @@
-package com.schedule;public class Schedule {
+package com.schedule;
+
+import com.service.PointService;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Schedule {
+    @Autowired
+    PointService pointService;
+
+    @Scheduled(fixedDelay = 10 * 1000)
+    public void updateStdValue(){
+        pointService.calculateStd();
+        System.out.println("Update StdValue...");
+    }
 }
